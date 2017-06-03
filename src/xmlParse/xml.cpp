e85ae2822ba9627e21ff6af8ed81ad31cbd1acd4
Henry Bergin <tackyattack@gmail.com>
2017-06-03 17:00:54 -0500

    xml parser updated

--- src/xmlParse/xml.cpp
+++ src/xmlParse/xml.cpp
@@ -22,41 +22,65 @@ void xmlParse::parseFile(string file_path)
 void xmlParse::buildXMLTree()
 {
     
-    xmlNode *currentNode = head; // points to the tree position
-    xmlNode *listNode = head; // points to the list position
-    xmlNode *newNode;
+    head = new xmlNode;
     
-    bool headFilled = false;
-    bool currentLevelFilled = false;
+    xmlNode *newNode;
+
     
     unsigned int refCount = 0;
     
-    while(!xmlFile.eof())
+    bool headFilled = false;
+    xmlNode *currentNode = head;
+    xmlNode *parentNode = head;
+    refCount = 0; // reset reference count
+    do
     {
         // go through first level
         
         char input;
         xmlFile >> input;
-        cout << input;
-        
+        //cout << input;
         
         if(input == '<')
         { // tag detected
+            // ignore other tags on next level (i.e. not 0 ref count)
+            if(refCount == 1)
+            {
+                cout << "open on current level" << endl;
+                cout << xmlFile.tellg() << endl;
+                if(!headFilled)
+                {
+                    head->file_position = xmlFile.tellg();
+                    currentNode->parent = parentNode;
+                    currentNode = head;
+                    headFilled = true;
+                }
+                else
+                {
+                    newNode = new xmlNode;
+                    newNode->file_position = xmlFile.tellg();
+                    newNode->parent = parentNode;
+                    currentNode->list_next = newNode;
+                    currentNode = currentNode->list_next;
+                }
+            }
             
             xmlFile >> input;
             if(input == '/')
             {
                 refCount--;
+                cout << "close tag" << endl;
             }
             else
             {
                 refCount++;
+                cout << "open tag" << endl;
             }
-            
+            cout << "ref count: " << refCount << endl;
             
         }
         
         
-    }
+    }while(refCount > 0);
     
 }
